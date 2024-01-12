ebf3892a4bb33054eb3ea1fa6e135c665baa408a
gideonjon <gideonjones63@gmail.com>
2024-01-12 23:25:55 +0100

    changes

---
+++ app/src/main/java/com/example/skilla/fragments/Space.kt
@@ -0,0 +1,23 @@
+package com.example.skilla.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.skilla.R
+
+class space : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_space, container, false)
+    }
+
+
+
+
+}
